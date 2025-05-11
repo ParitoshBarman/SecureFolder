62eef95862c79d5646992bea3bf599881a970654
Paritosh <barmanpari163@gmail.com>
2025-05-11 15:27:38 +0530

    Auto-commit at 11-05-2025 15:27:37.96

---
+++ Backend API for a Basic Blogging Platform with Role-Based Access Control/models/blogPostModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const blogPostSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    content:{
+        type:String,
+    },
+    author:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"author"
+    }
+})
+
+module.exports = mongoose.model("blogpost", blogPostSchema);
