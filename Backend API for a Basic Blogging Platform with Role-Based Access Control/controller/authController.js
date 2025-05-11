da6e24ba7ec182dcedf123c6d4aacee70bab1d57
Paritosh <barmanpari163@gmail.com>
2025-05-11 15:54:38 +0530

    Auto-commit at 11-05-2025 15:54:38.12

--- Backend API for a Basic Blogging Platform with Role-Based Access Control/controller/authController.js
+++ Backend API for a Basic Blogging Platform with Role-Based Access Control/controller/authController.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcrypt')
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 
 const Register = async (req, res) => {
@@ -9,25 +11,36 @@ const Register = async (req, res) => {
 
         let user = await User.create(bodyObject);
 
-        res.status(201).json({message:"User successfully registered", user});
-        
+        res.status(201).json({ message: "User successfully registered", user });
+
     } catch (error) {
         res.status(400).json(error);
-        
+
     }
-    
+
 }
 
 
 const Login = async (req, res) => {
     try {
-        
+        const { email, password } = req.body;
+        let user = await User.findOne({ email: email });
+        if (!user) {
+            return res.status(404).json({ error: "User not found" })
+        }
+        let compare = bcrypt.compareSync(password, user.password);
+        if (!compare) {
+            return res.status(400).json({ error: "Invalid password" })
+        }
+
+        let accessToken = jwt.sign({ id: user._id, email: user.email, role: user.role }, process.env.SECRET_KEY);
+        let refreshToken = jwt.sign({ id: user._id }, process.env.SECRET_KEY);
     } catch (error) {
         res.status(400).json(error);
-        
+
     }
-    
+
 }
 
 
-module.exports = { Register, Login}
+module.exports = { Register, Login }
