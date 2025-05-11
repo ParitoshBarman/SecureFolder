8bb9f737727c17fc200d7844e8a2d3b8386c3cfa
Paritosh <barmanpari163@gmail.com>
2025-05-11 15:42:38 +0530

    Auto-commit at 11-05-2025 15:42:38.01

--- Backend API for a Basic Blogging Platform with Role-Based Access Control/controller/authController.js
+++ Backend API for a Basic Blogging Platform with Role-Based Access Control/controller/authController.js
@@ -1,8 +1,15 @@
+const bcrypt = require('bcrypt')
 const User = require('../models/userModel')
 
 
 const Register = async (req, res) => {
     try {
+        let bodyObject = req.body;
+        bodyObject.password = bcrypt.hashSync(bodyObject.password, 10);
+
+        let user = await User.create(bodyObject);
+
+        res.status(201).json({message:"User successfully registered", user});
         
     } catch (error) {
         res.status(400).json(error);
