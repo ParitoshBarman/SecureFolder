de5ec4f507fe377abe4919ed4501289bcd188c4b
Paritosh <barmanpari163@gmail.com>
2025-05-11 16:30:37 +0530

    Auto-commit at 11-05-2025 16:30:37.89

--- Backend API for a Basic Blogging Platform with Role-Based Access Control/middleware/loggerMiddleware.js
+++ Backend API for a Basic Blogging Platform with Role-Based Access Control/middleware/loggerMiddleware.js
@@ -1,8 +1,11 @@
 const fs = require('fs')
+const path = require('path')
 
 const loger = async (req, res, next) => {
     try {
         console.log(req.method, req.url, Date.now())
+        // fs.appendFileSync()
+        console.log(__dirname)
         next();
         
     } catch (error) {
