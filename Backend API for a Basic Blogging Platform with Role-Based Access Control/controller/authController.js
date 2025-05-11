5d33065c6cbf2b955acf13fb4b9c7d7059bd30b6
Paritosh <barmanpari163@gmail.com>
2025-05-11 15:39:38 +0530

    Auto-commit at 11-05-2025 15:39:38.03

---
+++ Backend API for a Basic Blogging Platform with Role-Based Access Control/controller/authController.js
@@ -0,0 +1,26 @@
+const User = require('../models/userModel')
+
+
+const Register = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        res.status(400).json(error);
+        
+    }
+    
+}
+
+
+const Login = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        res.status(400).json(error);
+        
+    }
+    
+}
+
+
+module.exports = { Register, Login}
